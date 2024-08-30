c3282c1a6bf83179890ead4ad8e517b89f5deb82
Italoliveira <italo.moliveira@hotmail.com>
2024-08-30 14:28:55 -0300

    feat: Add Users Migrate, Seeders, Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Address;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = User::factory()
+        ->has(Address::factory()->count(1))
+        ->count(5)
+        ->create();
     }
 }
